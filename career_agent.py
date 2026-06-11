bcc062419c6dafcaf09675672966766773e0c190
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 03:59:30 +0530

    Update career_agent.py

--- career_agent.py
+++ career_agent.py
@@ -1,1 +1,31 @@
+import os
+import requests
 
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = """
+🚀 Career Agent Started
+
+Version 1 Active
+
+Sections:
+✅ Core Engineering Jobs
+✅ PSU Jobs
+✅ Government Exams
+✅ Remote Jobs
+
+More sources coming soon.
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+
+print("Message sent successfully")
