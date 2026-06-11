bc977ebf730fd55657e8cd3fc253c7c07cec7256
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 04:49:55 +0530

    Create psu.py

---
+++ sources/psu.py
@@ -0,0 +1,28 @@
+def get_psu_jobs():
+    return [
+        {
+            "title": "BEL Careers",
+            "score": "10/10",
+            "link": "https://bel-india.in/"
+        },
+        {
+            "title": "ECIL Careers",
+            "score": "10/10",
+            "link": "https://www.ecil.co.in/jobs.html"
+        },
+        {
+            "title": "ISRO Careers",
+            "score": "10/10",
+            "link": "https://www.isro.gov.in/Careers.html"
+        },
+        {
+            "title": "DRDO Careers",
+            "score": "10/10",
+            "link": "https://www.drdo.gov.in/careers"
+        },
+        {
+            "title": "NTPC Careers",
+            "score": "10/10",
+            "link": "https://careers.ntpc.co.in/"
+        }
+    ]
