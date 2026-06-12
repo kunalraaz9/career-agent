b43ddde79035a4cbeb989de7d4f029900e864641
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 14:27:18 +0530

    Create remote.py

---
+++ sources/remote.py
@@ -0,0 +1,5 @@
+def get_remote_jobs():
+    return [
+        ("Remote Engineering Jobs", "https://remoteok.com/", "8/10"),
+        ("Worldwide Remote Careers", "https://weworkremotely.com/", "8/10")
+    ]
