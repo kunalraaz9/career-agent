f41501b25c62438ab5a04b895362ab44ef8082e7
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 17:06:06 +0530

    Update core.py

--- sources/core.py
+++ sources/core.py
@@ -6,5 +6,16 @@ def get_core_jobs():
         ("Hitachi Energy", "https://www.hitachienergy.com/careers", "10/10"),
         ("GE Vernova", "https://www.gevernova.com/careers", "9/10"),
         ("Eaton Careers", "https://jobs.eaton.com", "9/10"),
-        ("L&T Careers", "https://larsentoubrocareers.peoplestrong.com", "9/10")
+        ("L&T Careers", "https://larsentoubrocareers.peoplestrong.com", "9/10"),
+
+        ("Texas Instruments", "https://careers.ti.com", "10/10"),
+        ("Intel Careers", "https://jobs.intel.com", "10/10"),
+        ("Qualcomm Careers", "https://www.qualcomm.com/company/careers", "10/10"),
+        ("NVIDIA Careers", "https://www.nvidia.com/en-in/about-nvidia/careers", "10/10"),
+        ("AMD Careers", "https://careers.amd.com", "10/10"),
+        ("Micron Careers", "https://careers.micron.com", "10/10"),
+        ("NXP Careers", "https://www.nxp.com/company/about-nxp/careers", "10/10"),
+        ("Infineon Careers", "https://www.infineon.com/careers", "10/10"),
+        ("Renesas Careers", "https://careers.renesas.com", "9/10"),
+        ("STMicroelectronics", "https://careers.st.com", "9/10")
     ]
