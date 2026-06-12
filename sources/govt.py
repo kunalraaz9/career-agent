c9b99cfee8839cfe93565947f687397e6ebc243f
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 14:26:45 +0530

    Create govt.py

---
+++ sources/govt.py
@@ -0,0 +1,7 @@
+def get_govt_jobs():
+    return [
+        ("SSC JE", "https://ssc.gov.in/", "10/10"),
+        ("RRB JE", "https://www.rrbcdg.gov.in/", "10/10"),
+        ("UPSC ESE", "https://upsc.gov.in/", "10/10"),
+        ("State AE/JE", "Various State PSC Websites", "8/10")
+    ]
