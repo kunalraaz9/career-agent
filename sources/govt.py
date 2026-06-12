228d6cd7580ce8f20d4473bb43e92be9535d855c
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 15:58:17 +0530

    Update govt.py

--- sources/govt.py
+++ sources/govt.py
@@ -1,7 +1,9 @@
 def get_govt_jobs():
     return [
-        ("SSC JE", "https://ssc.gov.in/", "10/10"),
-        ("RRB JE", "https://www.rrbcdg.gov.in/", "10/10"),
-        ("UPSC ESE", "https://upsc.gov.in/", "10/10"),
-        ("State AE/JE", "Various State PSC Websites", "8/10")
+        ("SSC JE", "https://ssc.gov.in", "10/10"),
+        ("RRB JE", "https://indianrailways.gov.in", "10/10"),
+        ("UPSC ESE", "https://upsc.gov.in", "10/10"),
+        ("BPSC AE", "https://bpsc.bih.nic.in", "9/10"),
+        ("JPSC AE", "https://jpsc.gov.in", "9/10"),
+        ("UPPSC AE", "https://uppsc.up.nic.in", "9/10")
     ]
