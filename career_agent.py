48298cb6dbcc3aecce59ebad1609146df62a7bcc
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 14:27:40 +0530

    Update career_agent.py

--- career_agent.py
+++ career_agent.py
@@ -1,10 +1,26 @@
 import os
 import requests
 
+from sources.psu import get_psu_jobs
+from sources.govt import get_govt_jobs
+from sources.remote import get_remote_jobs
+
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-message = "🚀 TEST MESSAGE FROM GITHUB ACTIONS"
+message = "🚀 DAILY CAREER REPORT\n\n"
+
+message += "🏛 PSU JOBS\n"
+for name, link, score in get_psu_jobs():
+    message += f"\n{name}\nScore: {score}\n{link}\n"
+
+message += "\n\n📢 GOVERNMENT EXAMS\n"
+for name, link, score in get_govt_jobs():
+    message += f"\n{name}\nScore: {score}\n{link}\n"
+
+message += "\n\n🌍 REMOTE JOBS\n"
+for name, link, score in get_remote_jobs():
+    message += f"\n{name}\nScore: {score}\n{link}\n"
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
@@ -16,5 +32,4 @@ response = requests.post(
     }
 )
 
-
-print("HELLO KUNAL 999")
+print(response.text)
