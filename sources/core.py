3ac55346315c06eba597aa9060a100125e973abe
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 15:53:36 +0530

    Create core.py

---
+++ sources/core.py
@@ -0,0 +1,10 @@
+def get_core_jobs():
+    return [
+        ("Siemens Careers", "https://jobs.siemens.com", "10/10"),
+        ("ABB Careers", "https://global.abb/group/en/careers", "10/10"),
+        ("Schneider Electric", "https://careers.se.com", "10/10"),
+        ("Hitachi Energy", "https://www.hitachienergy.com/careers", "10/10"),
+        ("GE Vernova", "https://www.gevernova.com/careers", "9/10"),
+        ("Eaton Careers", "https://jobs.eaton.com", "9/10"),
+        ("L&T Careers", "https://larsentoubrocareers.peoplestrong.com", "9/10")
+    ]
