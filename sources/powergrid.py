6c64e3e188a08e2bfd19f2fcd536be25f8a4c634
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-12 16:17:43 +0530

    Create powergrid.py

---
+++ sources/powergrid.py
@@ -0,0 +1,8 @@
+def get_powergrid_jobs():
+    return [
+        (
+            "POWERGRID Careers",
+            "https://www.powergrid.in/career-opportunities",
+            "10/10"
+        )
+    ]
