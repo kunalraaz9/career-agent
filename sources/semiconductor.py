8beed8700cfa0f4ef2def930ab3f2d62500716a1
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-13 11:34:34 +0530

    Create semiconductor.py

---
+++ sources/semiconductor.py
@@ -0,0 +1,13 @@
+def get_semiconductor_jobs():
+return [
+("Intel Careers", "https://jobs.intel.com", "10/10"),
+("Qualcomm Careers", "https://www.qualcomm.com/company/careers", "10/10"),
+("NVIDIA Careers", "https://www.nvidia.com/en-in/about-nvidia/careers", "10/10"),
+("AMD Careers", "https://careers.amd.com", "10/10"),
+("Micron Careers", "https://careers.micron.com", "10/10"),
+("NXP Careers", "https://www.nxp.com/company/about-nxp/careers", "10/10"),
+("Infineon Careers", "https://www.infineon.com/careers", "10/10"),
+("Texas Instruments", "https://careers.ti.com", "10/10"),
+("STMicroelectronics", "https://careers.st.com", "9/10"),
+("Renesas Careers", "https://careers.renesas.com", "9/10")
+]
