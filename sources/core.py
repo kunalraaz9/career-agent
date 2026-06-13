335b3b28b58a10d0ecbe41bc8489f32ebb7458ed
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-13 10:47:20 +0530

    Update core.py

--- sources/core.py
+++ sources/core.py
@@ -1,45 +1,20 @@
 def get_core_jobs():
-return [
-# Electrical / Power
-
-```("KUNAL TEST COMPANY", "https://google.com", "10/10"),
-    ("Siemens Careers", "https://jobs.siemens.com", "10/10"),
-    ("ABB Careers", "https://global.abb/group/en/careers", "10/10"),
-    ("Schneider Electric", "https://careers.se.com", "10/10"),
-    ("Hitachi Energy", "https://www.hitachienergy.com/careers", "10/10"),
-    ("GE Vernova", "https://www.gevernova.com/careers", "9/10"),
-    ("Eaton Careers", "https://jobs.eaton.com", "9/10"),
-    ("L&T Careers", "https://larsentoubrocareers.peoplestrong.com", "9/10"),
-    ("Tata Power Careers", "https://www.tatapower.com/careers", "9/10"),
-    ("Adani Energy Careers", "https://careers.adani.com", "8/10"),
-    ("JSW Energy Careers", "https://www.jsw.in/careers", "8/10"),
-
-    # Semiconductor
-
-    ("Texas Instruments", "https://careers.ti.com", "10/10"),
-    ("Intel Careers", "https://jobs.intel.com", "10/10"),
-    ("Qualcomm Careers", "https://www.qualcomm.com/company/careers", "10/10"),
-    ("NVIDIA Careers", "https://www.nvidia.com/en-in/about-nvidia/careers", "10/10"),
-    ("AMD Careers", "https://careers.amd.com", "10/10"),
-    ("Micron Careers", "https://careers.micron.com", "10/10"),
-    ("NXP Careers", "https://www.nxp.com/company/about-nxp/careers", "10/10"),
-    ("Infineon Careers", "https://www.infineon.com/careers", "10/10"),
-    ("Renesas Careers", "https://careers.renesas.com", "9/10"),
-    ("STMicroelectronics", "https://careers.st.com", "9/10"),
-
-    # EDA / VLSI
-
-    ("Synopsys Careers", "https://careers.synopsys.com", "10/10"),
-    ("Cadence Careers", "https://cadence.wd1.myworkdayjobs.com", "10/10"),
-    ("Siemens EDA", "https://eda.sw.siemens.com", "10/10"),
-
-    # Embedded / Electronics
-
-    ("Honeywell Careers", "https://careers.honeywell.com", "9/10"),
-    ("Bosch Careers", "https://www.bosch.com/careers", "9/10"),
-    ("Continental Careers", "https://www.continental.com/careers", "9/10"),
-    ("Analog Devices", "https://www.analog.com/careers", "9/10"),
-    ("Broadcom Careers", "https://careers.broadcom.com", "9/10"),
-    ("MediaTek Careers", "https://careers.mediatek.com", "9/10")
-]
-```
+    return [
+        ("Siemens Careers", "https://jobs.siemens.com", "10/10"),
+        ("ABB Careers", "https://global.abb/group/en/careers", "10/10"),
+        ("Schneider Electric", "https://careers.se.com", "10/10"),
+        ("Hitachi Energy", "https://www.hitachienergy.com/careers", "10/10"),
+        ("GE Vernova", "https://www.gevernova.com/careers", "9/10"),
+        ("Eaton Careers", "https://jobs.eaton.com", "9/10"),
+        ("L&T Careers", "https://larsentoubrocareers.peoplestrong.com", "9/10"),
+        ("Texas Instruments", "https://careers.ti.com", "10/10"),
+        ("Intel Careers", "https://jobs.intel.com", "10/10"),
+        ("Qualcomm Careers", "https://www.qualcomm.com/company/careers", "10/10"),
+        ("NVIDIA Careers", "https://www.nvidia.com/en-in/about-nvidia/careers", "10/10"),
+        ("AMD Careers", "https://careers.amd.com", "10/10"),
+        ("Micron Careers", "https://careers.micron.com", "10/10"),
+        ("NXP Careers", "https://www.nxp.com/company/about-nxp/careers", "10/10"),
+        ("Infineon Careers", "https://www.infineon.com/careers", "10/10"),
+        ("Renesas Careers", "https://careers.renesas.com", "9/10"),
+        ("STMicroelectronics", "https://careers.st.com", "9/10")
+    ]
