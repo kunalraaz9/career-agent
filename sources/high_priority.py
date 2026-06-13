caf6167cd39db61b0940e41ebf9a02df131896e7
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-13 11:37:13 +0530

    Create high_priority.py

---
+++ sources/high_priority.py
@@ -0,0 +1,12 @@
+def get_high_priority():
+return [
+("BEL", "PSU", "10/10"),
+("ECIL", "PSU", "10/10"),
+("NTPC", "PSU", "10/10"),
+("POWERGRID", "PSU", "10/10"),
+("SSC JE", "Exam", "10/10"),
+("RRB JE", "Exam", "10/10"),
+("Intel", "Semiconductor", "10/10"),
+("Qualcomm", "Semiconductor", "10/10"),
+("NXP", "Semiconductor", "10/10")
+]
