c041397bebf7849394e6dfe4b3e3339779eb3d4d
kunalraaz9 <kunalraaz9@gmail.com>
2026-06-13 10:58:47 +0530

    Create live_jobs.py

---
+++ sources/live_jobs.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_live_test():
+try:
+response = requests.get("https://www.ecil.co.in/jobs.html", timeout=15)
+
+```
+    return [
+        (
+            "ECIL Website Reachable",
+            "https://www.ecil.co.in/jobs.html",
+            "10/10"
+        )
+    ]
+
+except Exception as e:
+    return [
+        (
+            f"ERROR: {e}",
+            "",
+            "0/10"
+        )
+    ]
+```
